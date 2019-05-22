67a8f32c0bc0cc2627ba8d0a585d973fc6be6ab8
Monika Dubel <45172904+monikadubel@users.noreply.github.com>
2019-05-22 12:09:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ callingCodesAll.sort(function (a, b) {
       return -1;
     } else if (splitA[1] > splitB[1]) {
       return 1;
-    } else if (splitA[1] = splitB[1]) {
+    } else if (splitA[1] == splitB[1]) {
       return 1;
     } else {
       return 0;
